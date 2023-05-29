58fd964e996aba2291d63706e3f712ff43ae9b83
divyanshu.goel <divyanshu.goel@addverb.com>
2023-05-30 01:39:23 +0530

    changes for protoc

--- protobufGen/share/MacroProtocGen.cmake
+++ protobufGen/share/MacroProtocGen.cmake
@@ -12,7 +12,21 @@ function(PROTOC_GEN protocfilenameArg protofolderNameArg outPath)
 	set(PROTOC_EXECECUTABLE ${PROTOBUF_PROTOC_EXEC}
 		${protocfilenameArg}
 		--proto_path=${protofolderNameArg}
+		--plugin=protoc-gen-grpc=${GRPC_PLUGIN_EXEC_CPP}
+		--grpc_out=${outPath}
 		--cpp_out=${outPath})
 	project_execute_process(COMMAND "${PROTOC_EXECECUTABLE}")
 endfunction(PROTOC_GEN)
+
+
+function(PROTOC_GEN_PY protocfilenameArg protofolderNameArg outPath)
+	file(MAKE_DIRECTORY ${outPath})
+	set(PROTOC_EXECECUTABLE ${PROTOBUF_PROTOC_EXEC}
+		${protocfilenameArg}
+		--proto_path=${protofolderNameArg}
+		--plugin=protoc-gen-grpc_python=${GRPC_PLUGIN_EXEC_PY}
+		--grpc_python_out=${outPath}
+		--python_out=${outPath})
+	project_execute_process(COMMAND "${PROTOC_EXECECUTABLE}")
+endfunction(PROTOC_GEN_PY)
 
