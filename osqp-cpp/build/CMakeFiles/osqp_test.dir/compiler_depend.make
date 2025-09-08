22d6d83c5a3bb3b04608300f8c4e9997e2efe09f
Aditya Sharma <aditya.sharma@addverb.com>
2025-09-08 16:47:46 +0530

    Added support for osqp-cpp

---
+++ osqp-cpp/build/CMakeFiles/osqp_test.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for osqp_test.
+# This may be replaced when dependencies are built.
---
+++ osqp-cpp/build/CMakeFiles/osqp_test.dir/link.d
@@ -0,0 +1,244 @@
+osqp_test: \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o \
+  CMakeFiles/osqp_test.dir/test/osqp++_test.cc.o \
+  lib/libgtest.a \
+  lib/libgmock.a \
+  lib/libgtest_main.a \
+  _deps/abseil-cpp-build/absl/status/libabsl_status.a \
+  libosqp-cpp.a \
+  lib/libgtest.a \
+  _deps/abseil-cpp-build/absl/status/libabsl_statusor.a \
+  _deps/abseil-cpp-build/absl/status/libabsl_status.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_cord.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_cordz_info.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_cord_internal.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_cordz_functions.a \
+  _deps/abseil-cpp-build/absl/profiling/libabsl_exponential_biased.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_cordz_handle.a \
+  _deps/abseil-cpp-build/absl/synchronization/libabsl_synchronization.a \
+  _deps/abseil-cpp-build/absl/synchronization/libabsl_graphcycles_internal.a \
+  _deps/abseil-cpp-build/absl/synchronization/libabsl_kernel_timeout_internal.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_tracing_internal.a \
+  _deps/abseil-cpp-build/absl/time/libabsl_time.a \
+  _deps/abseil-cpp-build/absl/time/libabsl_civil_time.a \
+  _deps/abseil-cpp-build/absl/time/libabsl_time_zone.a \
+  _deps/abseil-cpp-build/absl/crc/libabsl_crc_cord_state.a \
+  _deps/abseil-cpp-build/absl/crc/libabsl_crc32c.a \
+  _deps/abseil-cpp-build/absl/crc/libabsl_crc_internal.a \
+  _deps/abseil-cpp-build/absl/crc/libabsl_crc_cpu_detect.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_leak_check.a \
+  _deps/abseil-cpp-build/absl/types/libabsl_bad_optional_access.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_stacktrace.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_strerror.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_symbolize.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_debugging_internal.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_demangle_internal.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_demangle_rust.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_decode_rust_punycode.a \
+  _deps/abseil-cpp-build/absl/debugging/libabsl_utf8_for_code_point.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_malloc_internal.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_str_format_internal.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_strings.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_strings_internal.a \
+  _deps/abseil-cpp-build/absl/strings/libabsl_string_view.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_throw_delegate.a \
+  _deps/abseil-cpp-build/absl/numeric/libabsl_int128.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_base.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_spinlock_wait.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/librt.so \
+  _deps/abseil-cpp-build/absl/types/libabsl_bad_variant_access.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_raw_logging_internal.a \
+  _deps/abseil-cpp-build/absl/base/libabsl_log_severity.a \
+  _deps/osqp-build/out/libosqp.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libdl.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so \
+  /lib/x86_64-linux-gnu/libm.so.6 \
+  /lib/x86_64-linux-gnu/libmvec.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so \
+  /lib/x86_64-linux-gnu/libc.so.6 \
+  /usr/lib/x86_64-linux-gnu/libc_nonshared.a \
+  /lib64/ld-linux-x86-64.so.2 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1 \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a \
+  /usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o \
+  /usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o \
+  /lib64/ld-linux-x86-64.so.2
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/Scrt1.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crti.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtbeginS.o:
+
+CMakeFiles/osqp_test.dir/test/osqp++_test.cc.o:
+
+lib/libgtest.a:
+
+lib/libgmock.a:
+
+lib/libgtest_main.a:
+
+_deps/abseil-cpp-build/absl/status/libabsl_status.a:
+
+libosqp-cpp.a:
+
+lib/libgtest.a:
+
+_deps/abseil-cpp-build/absl/status/libabsl_statusor.a:
+
+_deps/abseil-cpp-build/absl/status/libabsl_status.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_cord.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_cordz_info.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_cord_internal.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_cordz_functions.a:
+
+_deps/abseil-cpp-build/absl/profiling/libabsl_exponential_biased.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_cordz_handle.a:
+
+_deps/abseil-cpp-build/absl/synchronization/libabsl_synchronization.a:
+
+_deps/abseil-cpp-build/absl/synchronization/libabsl_graphcycles_internal.a:
+
+_deps/abseil-cpp-build/absl/synchronization/libabsl_kernel_timeout_internal.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_tracing_internal.a:
+
+_deps/abseil-cpp-build/absl/time/libabsl_time.a:
+
+_deps/abseil-cpp-build/absl/time/libabsl_civil_time.a:
+
+_deps/abseil-cpp-build/absl/time/libabsl_time_zone.a:
+
+_deps/abseil-cpp-build/absl/crc/libabsl_crc_cord_state.a:
+
+_deps/abseil-cpp-build/absl/crc/libabsl_crc32c.a:
+
+_deps/abseil-cpp-build/absl/crc/libabsl_crc_internal.a:
+
+_deps/abseil-cpp-build/absl/crc/libabsl_crc_cpu_detect.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_leak_check.a:
+
+_deps/abseil-cpp-build/absl/types/libabsl_bad_optional_access.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_stacktrace.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_strerror.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_symbolize.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_debugging_internal.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_demangle_internal.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_demangle_rust.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_decode_rust_punycode.a:
+
+_deps/abseil-cpp-build/absl/debugging/libabsl_utf8_for_code_point.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_malloc_internal.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_str_format_internal.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_strings.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_strings_internal.a:
+
+_deps/abseil-cpp-build/absl/strings/libabsl_string_view.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_throw_delegate.a:
+
+_deps/abseil-cpp-build/absl/numeric/libabsl_int128.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_base.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_spinlock_wait.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/librt.so:
+
+_deps/abseil-cpp-build/absl/types/libabsl_bad_variant_access.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_raw_logging_internal.a:
+
+_deps/abseil-cpp-build/absl/base/libabsl_log_severity.a:
+
+_deps/osqp-build/out/libosqp.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libdl.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libstdc++.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libm.so:
+
+/lib/x86_64-linux-gnu/libm.so.6:
+
+/lib/x86_64-linux-gnu/libmvec.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libc.so:
+
+/lib/x86_64-linux-gnu/libc.so.6:
+
+/usr/lib/x86_64-linux-gnu/libc_nonshared.a:
+
+/lib64/ld-linux-x86-64.so.2:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc_s.so:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/libgcc_s.so.1:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/libgcc.a:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/crtendS.o:
+
+/usr/lib/gcc/x86_64-linux-gnu/13/../../../x86_64-linux-gnu/crtn.o:
+
+/lib64/ld-linux-x86-64.so.2:
